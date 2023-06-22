3b8022b9e209a6b6d7ff02737eb39018926ed6b3
developer-talaghay <developer@talaghay.com>
2023-06-22 21:07:18 +0800

    added signup endpoint

--- app/controllers/userSignupController.js
+++ app/controllers/userSignupController.js
@@ -3,22 +3,49 @@ const UserModel = require("../models/userSignupModel");
 exports.createUser = function (req, res) {
   const newUser = {
     email_add: req.body.email_add,
+    username: req.body.username,
+    name: req.body.name,
     password: req.body.password,
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    birth_date: req.body.birth_date,
-    country: req.body.country,
-    phone_number: req.body.phone_number
+    category: req.body.category,
+    contact_number: req.body.contact_number
   };
+
+  // Validate the category field
+  if (newUser.category !== "user" && newUser.category !== "guardian") {
+    console.error('Error creating user: invalid category');
+    return res.status(400).json({
+      error: 'Invalid category',
+      message: 'Only accepts: guardian or user'
+    });
+  }
 
   UserModel.create(newUser, (error, result) => {
     if (error) {
-      if (error === 'Email address already exists') {
+      if (error === 'Email address and username already exist') {
+        console.error('Error creating user: email and username already exist');
+        return res.status(409).json({
+          error: 'Email address and username already exist',
+          message: 'Error creating user: email and username already exist'
+        });
+      } else if (error === 'Email address already exists') {
         console.error('Error creating user: email already exists');
-        return res.status(409).send('Email address already exists');
+        return res.status(409).json({
+          error: 'Email address already exists',
+          message: 'Error creating user: email already exists'
+        });
+      } else if (error === 'Username already exists') {
+        console.error('Error creating user: username already exists');
+        return res.status(409).json({
+          error: 'Username already exists',
+          message: 'Error creating user: username already exists'
+        });
+      } else {
+        console.error("Error creating user: ", error);
+        return res.status(500).json({
+          error: 'Internal Server Error',
+          message: 'Error creating user'
+        });
       }
-      console.error("Error creating user: ", error);
-      res.status(500).send("Error creating user");
     } else {
       console.log("User created successfully");
       res.status(201).json({
@@ -27,4 +54,4 @@ exports.createUser = function (req, res) {
       });
     }
   });
-};  
+};
--- app/models/userSignupModel.js
+++ app/models/userSignupModel.js
@@ -4,17 +4,24 @@ const bcrypt = require('bcrypt');
 const User = {};
 
 User.create = (newUser, callback) => {
-  // Check if the email address already exists
+  // Check if the email address or username already exists
   dbConn.query(
-    "SELECT * FROM user WHERE email_add = ?",
-    [newUser.email_add],
+    "SELECT * FROM user WHERE email_add = ? OR username = ?",
+    [newUser.email_add, newUser.username],
     (error, result) => {
       if (error) {
-        console.error("Error checking email address existence: ", error);
+        console.error("Error checking email address and username existence: ", error);
         return callback(error, null);
       } else if (result.length > 0) {
-        // Email address already exists
-        return callback("Email address already exists", null);
+        // Email address or username already exists
+        const existingUser = result[0];
+        if (existingUser.email_add === newUser.email_add && existingUser.username === newUser.username) {
+          return callback("Email address and username already exist", null);
+        } else if (existingUser.email_add === newUser.email_add) {
+          return callback("Email address already exists", null);
+        } else if (existingUser.username === newUser.username) {
+          return callback("Username already exists", null);
+        }
       } else {
         // Hash the password
         const saltRounds = 10;
@@ -24,10 +31,10 @@ User.create = (newUser, callback) => {
             console.error("Error hashing password: ", err);
             return callback(err, null);
           } else {
-            // Insert the user into the database
+            // Insert the user into the 'user' table
             dbConn.query(
-              "INSERT INTO user (email_add, password) VALUES (?, ?)",
-              [newUser.email_add, hashedPassword],
+              "INSERT INTO user (email_add, username, password, category) VALUES (?, ?, ?, ?)",
+              [newUser.email_add, newUser.username, hashedPassword, newUser.category],
               (error, result) => {
                 if (error) {
                   console.error("Error inserting user into database: ", error);
@@ -37,8 +44,8 @@ User.create = (newUser, callback) => {
 
                   // Insert user details into the 'user_details' table
                   dbConn.query(
-                    "INSERT INTO user_details (user_id, first_name, last_name, birth_date, country, phone_number) VALUES (?, ?, ?, ?, ?, ?)",
-                    [userId, newUser.first_name, newUser.last_name, newUser.birth_date, newUser.country, newUser.phone_number],
+                    "INSERT INTO user_details (user_id, phone_number, name) VALUES (?, ?, ?)",
+                    [userId, newUser.contact_number, newUser.name],
                     (error, result) => {
                       if (error) {
                         console.error("Error inserting user details into database: ", error);
@@ -57,6 +64,5 @@ User.create = (newUser, callback) => {
     }
   );
 };
-
 
 module.exports = User;
