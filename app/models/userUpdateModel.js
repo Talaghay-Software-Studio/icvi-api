68e3a1f8252bc298885b4ef841c8290bc0711776
developer-talaghay <developer@talaghay.com>
2023-10-02 18:22:49 +0800

    updated the update user

--- app/models/userUpdateModel.js
+++ app/models/userUpdateModel.js
@@ -4,70 +4,79 @@ const dbConn = require("../config/db.config");
 const UpdatedUser = {};
 
 UpdatedUser.update = (userId, updatedUser, gender, impairmentCategory, callback) => {
-  // Check if the user with the provided userId exists
+  // Check if the user with the provided userId exists in the user_details table
   dbConn.query(
-    "SELECT * FROM user WHERE id = ?",
+    "SELECT * FROM user_details WHERE user_id = ?",
     [userId],
     (error, result) => {
       if (error) {
-        console.error("Error checking user existence: ", error);
+        console.error("Error checking user details existence: ", error);
         return callback(error, null);
-      } else if (result.length === 0) {
-        // User with the provided userId doesn't exist
-        return callback("User ID doesn't exist", null);
       } else {
-        // Proceed with updating the user
-        // Check if the email address or username already exists
-        dbConn.query(
-          "SELECT * FROM user WHERE (email_add = ? OR username = ?) AND id <> ?",
-          [updatedUser.email_add, updatedUser.username, userId],
-          (error, result) => {
-            if (error) {
-              console.error("Error checking email address and username existence: ", error);
-              return callback(error, null);
-            } else if (result.length > 0) {
-              // Email address or username already exists
-              const existingUser = result[0];
-              if (existingUser.email_add === updatedUser.email_add && existingUser.username === updatedUser.username) {
-                return callback("Email address and username already exist", null);
-              } else if (existingUser.email_add === updatedUser.email_add) {
-                return callback("Email address already exists", null);
-              } else if (existingUser.username === updatedUser.username) {
-                return callback("Username already exists", null);
-              }
-            } else {
-              // Update the user in the 'user' table (without password)
-              dbConn.query(
-                "UPDATE user SET username = ? WHERE id = ?",
-                [updatedUser.username, userId],
-                (error, result) => {
-                  if (error) {
-                    console.error("Error updating user in database: ", error);
-                    return callback(error, null);
-                  } else {
-                    // Update user details in the 'user_details' table
-                    dbConn.query(
-                      "UPDATE user_details SET phone_number = ?, name = ?, gender = ?, impairment_category = ?, age = ?, address = ? WHERE user_id = ?",
-                      [updatedUser.phone_number, updatedUser.name, gender, impairmentCategory, updatedUser.age, updatedUser.address, userId],
-                      (error, result) => {
-                        if (error) {
-                          console.error("Error updating user details in database: ", error);
-                          return callback(error, null);
-                        } else {
-                          return callback(null, result);
-                        }
-                      }
-                    );
+        // Check if the user details exist
+        const userDetailsExist = result.length > 0;
+
+        // If the user details don't exist, insert a new row in the 'user_details' table
+        if (!userDetailsExist) {
+          dbConn.query(
+            "INSERT INTO user_details (user_id, phone_number, name, gender, impairment_category, age, address) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?)",
+            [userId, updatedUser.phone_number, updatedUser.name, gender, impairmentCategory, updatedUser.age, updatedUser.address],
+            (error, result) => {
+              if (error) {
+                console.error("Error inserting new user details: ", error);
+                return callback(error, null);
+              } else {
+                // Update the 'user' table's 'username' column
+                dbConn.query(
+                  "UPDATE user SET username = ? WHERE id = ?",
+                  [updatedUser.username, userId],
+                  (error, result) => {
+                    if (error) {
+                      console.error("Error updating username in the user table: ", error);
+                      return callback(error, null);
+                    } else {
+                      return callback(null, result);
+                    }
                   }
-                }
-              );
+                );
+              }
             }
-          }
-        );
+          );
+        } else {
+          // Proceed with updating the user details in the 'user_details' table
+          dbConn.query(
+            "UPDATE user_details SET phone_number = ?, name = ?, gender = ?, impairment_category = ?, age = ?, address = ? WHERE user_id = ?",
+            [updatedUser.phone_number, updatedUser.name, gender, impairmentCategory, updatedUser.age, updatedUser.address, userId],
+            (error, result) => {
+              if (error) {
+                console.error("Error updating user details in database: ", error);
+                return callback(error, null);
+              } else {
+                // Update the 'user' table's 'username' column
+                dbConn.query(
+                  "UPDATE user SET username = ? WHERE id = ?",
+                  [updatedUser.username, userId],
+                  (error, result) => {
+                    if (error) {
+                      console.error("Error updating username in the user table: ", error);
+                      return callback(error, null);
+                    } else {
+                      return callback(null, result);
+                    }
+                  }
+                );
+              }
+            }
+          );
+        }
       }
     }
   );
 };
+
+
+
 
 UpdatedUser.updateWithoutGenderAndImpairment = (userId, updatedUser, callback) => {
   // Check if the user with the provided userId exists
